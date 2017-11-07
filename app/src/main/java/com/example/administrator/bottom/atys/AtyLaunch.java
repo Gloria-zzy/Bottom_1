7e277e812e9b456b985d37ba3aacd9c22d12725c
Administrator <996496374@qq.com>
2017-11-07 21:38:35 +0800

    Add launch page
    First enter launch page. Waiting for 2 sec then enter login page.

---
+++ app/src/main/java/com/example/administrator/bottom/atys/AtyLaunch.java
@@ -0,0 +1,30 @@
+package com.example.administrator.bottom.atys;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.bottom.MainActivity;
+import com.example.administrator.bottom.R;
+
+public class AtyLaunch extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //加载启动图片
+        setContentView(R.layout.aty_launch);
+        Integer time = 2000;    //设置等待时间，单位为毫秒
+        Handler handler = new Handler();
+        //当计时结束时，跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(AtyLaunch.this, AtyLogin.class));
+                AtyLaunch.this.finish();
+            }
+        }, time);
+    }
+}
