1017ed8a77b727d3fcdc89e958ccc979b048e4e0
Administrator <996496374@qq.com>
2017-11-11 00:21:14 +0800

    Add simple fetch layout

--- app/src/main/java/com/example/administrator/bottom/atys/AtyMainFrame.java
+++ app/src/main/java/com/example/administrator/bottom/atys/AtyMainFrame.java
@@ -2,6 +2,7 @@ package com.example.administrator.bottom.atys;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
--- app/src/main/java/com/example/administrator/bottom/frag/FragHome.java
+++ app/src/main/java/com/example/administrator/bottom/frag/FragHome.java
@@ -1,15 +1,22 @@
 package com.example.administrator.bottom.frag;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.bottom.R;
+import com.example.administrator.bottom.atys.AtyFetch;
+import com.example.administrator.bottom.atys.AtyLogin;
+import com.example.administrator.bottom.atys.AtyRegister;
 
 /**
  * Created by Administrator on 2017/10/29.
@@ -18,6 +25,7 @@ import com.example.administrator.bottom.R;
 public class FragHome extends Fragment {
     private String context;
     private TextView mTextView;
+    private Button btn;
 
     public FragHome(){
 
@@ -28,8 +36,22 @@ public class FragHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_home,container,false);
-//        mTextView = (TextView)view.findViewById(R.id.txt_content);
-//        mTextView.setText(R.string.home);
+        btn=(Button)view.findViewById(R.id.get_btn);
         return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivityForResult(new Intent(getActivity(),AtyFetch.class), Activity.RESULT_FIRST_USER);
+            }
+        });
     }
 }
