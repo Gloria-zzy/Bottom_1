540d62b303708b5c9ee69c98f42918f5a8b25716
Administrator <996496374@qq.com>
2017-11-15 20:09:47 +0800

    Add frag_order and frag_community

--- app/src/main/java/com/example/administrator/bottom/frag/FragOrder.java
+++ app/src/main/java/com/example/administrator/bottom/frag/FragOrder.java
@@ -27,8 +27,9 @@ public class FragOrder extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_order,container,false);
-//        mTextView = (TextView)view.findViewById(R.id.txt_content);
-//        mTextView.setText(R.string.get);
+
+
+
         return view;
     }
 
